6df552e5a396a90192bd7bedf0611c8c3ee3aea1
10041 <andreyoleksyuk004@gmail.com>
2017-11-30 23:29:31 +0300

    add dirs.js

---
+++ dirs.js
@@ -0,0 +1,37 @@
+const Promise = require("bluebird");
+const fs = Promise.promisifyAll(require("fs"));
+const path = require("path");
+
+const dirs = [
+    'dirs/dir-1/dir-1-1',
+    'dirs/dir-1/dir-1-2',
+    'dirs/dir-1/dir-1-2/dir-1-2-1',
+    'dirs/dir-2/dir-2-1/dir-2-1-1',
+    'dirs/dir-2/dir-2-2/dir-2-2-1',
+    'dirs/dir-2/dir-2-1/dir-2-2-2/dir-2-2-2-1',
+    'dirs/dir-3/dir-3-1',
+    'dirs/dir-3',
+    'dirs/dir-3/dir-3-2/dir-3-2-1',
+    'dirs/dir-3/dir-3-3/dir-3-3-1'
+];
+
+Promise.mapSeries(dirs, (dirName) =>{
+    return dirName.split('/');
+})
+.then((response) =>{
+    for (let dir of response) {
+        let basesPath = './';
+        for (let path of dir) {
+            basesPath += path + '/';
+            if(!fs.existsSync(basesPath)){
+                fs.mkdirAsync(basesPath)
+                .catch((error) => {
+                    console.log(error);
+                });
+            }
+        }
+    }
+})
+.catch((error) => {
+    console.log(error)
+});
